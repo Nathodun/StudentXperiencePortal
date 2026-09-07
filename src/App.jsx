7c890e163e49988da5a2ebbc071e9c450bff8823
Nathodun <oduntannathaniel03@gmail.com>
2026-09-07 15:35:04 +0100

    feat: changes to logo and track case

--- src/App.jsx
+++ src/App.jsx
@@ -210,23 +210,22 @@ export default function App() {
       {}
       <header className="bg-white border-b border-slate-200 sticky top-0 z-30 shadow-sm">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-3.5 flex justify-between items-center">
-          
           {/* NBU Crest & Brand Title */}
-          <div className="flex items-center space-x-3">
-              <div className="flex items-center justify-start max-w-[150px] sm:max-w-[180px] h-auto">
-                <img 
-                src="/logo.png" 
-                alt="Student Experience Office Logo" 
-                className="w-full h-auto object-contain dynamic-logo-filter" 
-                  />
-              </div> <br>
-              <p className="text-xs font-semibold text-amber-600 mt-0.5 tracking-wider uppercase">
-                Office of the Student Experience Officer (SEO)
-              </p>
-          <div>
-              
-            </div>
-          </div>
+  <div className="flex flex-col sm:flex-row sm:items-center space-y-1 sm:space-y-0 sm:space-x-3">
+      <div className="flex items-center justify-start max-w-[150px] sm:max-w-[180px] h-auto">
+        <img 
+        src="/logo.png" 
+        alt="Student Experience Office Logo" 
+        className="w-full h-auto object-contain dynamic-logo-filter" 
+         />
+      </div>
+        <div>
+         <p className="text-xs font-semibold text-amber-600 mt-0.5 tracking-wider uppercase">
+         Office of the Student Experience Officer (SEO)
+         </p>
+        </div>
+      </div>
+  </div>
 
           {/* Action Navigation */}
           <div className="flex items-center space-x-3">
