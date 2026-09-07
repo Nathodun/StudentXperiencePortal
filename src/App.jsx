bc41b53534c414ee95ab3362c2702501fc5206d7
Nathodun <oduntannathaniel03@gmail.com>
2026-09-07 14:21:21 +0100

    feat: refining the landing page

--- src/App.jsx
+++ src/App.jsx
@@ -280,7 +280,7 @@ export default function App() {
                 
                 <button
                   onClick={() => setActiveModal('emergency')}
-                  className="w-full sm:w-auto bg-amber-600 hover:bg-amber-500 text-white text-xs font-bold px-4 py-2 rounded-lg transition-all text-center whitespace-nowrap shadow-md"
+                  className="flex items-center w-full sm:w-auto bg-amber-600 hover:bg-amber-500 text-white text-xs font-bold px-4 py-2 rounded-lg transition-all text-center whitespace-nowrap shadow-md"
                 >
                   <Bell className="w-5 h-5" />
                   <span>Report an Incident</span>
